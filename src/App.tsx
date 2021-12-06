c7efc896ebbaa59bb13ad2ba122ab0e3afb7bdf4
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-12-05 22:27:06 -0500

    changed rules to only allow logged in users

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Status from './content/Status';
 import Payment from './content/Payment';
 import Transactions from './content/Transactions';
+import { useAuthStateHook } from './core/Firebase';
+import { Error403 } from './core/Errors';
 
 function App() {
+	const [user] = useAuthStateHook();
+	if (!user) return <Error403 />;
 	return (
 		<div>
 			<Status />
--- src/core/Errors.tsx
+++ src/core/Errors.tsx
@@ -4,7 +4,7 @@ export function Error403() {
 	return (
 		<div>
 			<h1 className='text-3xl m-2 p-3 bg-red-400 rounded-lg'>
-				Valid Account needed to view.
+				Account needed to view.
 			</h1>
 		</div>
 	);
